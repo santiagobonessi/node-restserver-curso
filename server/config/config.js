a5bc2ad43b30e2fdc660da85af64ef798713e466
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-03-07 18:18:10 -0300

    fix sintax

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = rocess.env.MONGO_URL;
+    urlDB = process.env.MONGO_URL;
 }
 process.env.URLDB = urlDB;
